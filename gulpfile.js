b44fe63ffb5833797100cccbb3b0e2c1011466ff
VorgSpring <drmrSP@yandex.ru>
2016-09-14 23:37:05 +0300

    created structure

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,14 @@ var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var jade = require('gulp-jade');
 var server = require("browser-sync");
 
 
 gulp.task('style', function () {
-    gulp.src('sass/style.sass')
-        .pipe(plumber()).pipe(sass())
+    gulp.src('sass/style.scss')
+        .pipe(plumber())
+        .pipe(sass())
         .pipe(postcss([
             autoprefixer({browsers: [
                 "last 1 version",
@@ -24,11 +26,20 @@ gulp.task('style', function () {
         .pipe(server.reload({stream: true}));
 });
 
+gulp.task('jade', function(){
+    gulp.src('./jade/index.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./'))
+});
+
 gulp.task("serve", ["style"], function() {
     server.init({
         server: "."
     });
     gulp.watch("sass/**/*.scss", ["style"]);
+    gulp.watch('jade/**/*.jade',['jade']);
     gulp.watch("*.html")
         .on("change", server.reload);
 });
